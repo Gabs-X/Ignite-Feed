89fafa70e9c2e3edf4ed411e6388b5bee7af6800
Gabriel Anthony Botelho <Ga13121110@gmail.com>
2023-03-28 12:50:01 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Header } from './components/Header';
 import { Post } from './Post'
 import './global.css';
+import styles from './App.module.css'
+
 
 function App() {
   
@@ -8,14 +10,7 @@ function App() {
     <div>
     <Header />
 
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem non maiores consequatur reprehenderit quia autem, suscipit eum omnis pariatur atque incidunt tenetur eius rerum facere tempora ab accusamus in. Nostrum!"
-      />
-      <Post 
-        author="Gabriel Buzzi"
-        content="Um novo post muito legal"      
-      />
+<div className='wrapper'></div>
 
     </div>
   )
